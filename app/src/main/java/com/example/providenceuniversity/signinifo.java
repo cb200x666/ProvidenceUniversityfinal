ef9a1abe800c6ce48027ab165825284c34d7ef6a
bryan sun <livess0601@gmail.com>
2022-10-06 01:40:47 +0800

    Initial commit

---
+++ app/src/main/java/com/example/providenceuniversity/signinifo.java
@@ -0,0 +1,13 @@
+package com.example.providenceuniversity;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class signinifo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signinifo);
+    }
+}
